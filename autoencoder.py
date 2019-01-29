de56da8e4f20e0823657d6fe54a1cdcc63c331dd
yoheitaonishi <onishiyoheita@gmail.com>
2019-01-29 15:25:23 +0900

    fix network

--- autoencoder.py
+++ autoencoder.py
@@ -2,13 +2,13 @@ from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
 from keras.models import Model
 from keras import backend as K
 from keras.callbacks import TensorBoard
+from keras.utils import plot_model
 from keras.preprocessing.image import load_img,img_to_array
 import numpy as np
 import glob
 from PIL import Image
 
 # Get images and convert
-
 IMAGE_FILE = './work/images/*.jpg'
 image_list = glob.glob(IMAGE_FILE)
 image_data_array = []
@@ -35,41 +35,17 @@ x = Conv2D(16, (3, 3), activation='relu', padding='same')(encoded)
 x = UpSampling2D((2, 2))(x)
 x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
 x = UpSampling2D((2, 2))(x)
-x = Conv2D(64, (3, 3), activation='relu')(x)
+x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
 x = UpSampling2D((2, 2))(x)
 decoded = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(x)
 
 autoencoder = Model(input_img, decoded)
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
-
-
-
 autoencoder.fit(image_data_array, image_data_array,
                 epochs=50,
                 batch_size=128,
                 shuffle=True,
                 validation_data=(image_data_array, image_data_array),
-                callbacks=[TensorBoard(log_dir='/logs/log.log')])
-
-
-
-
-
-
-
-
-"""
-Get image file and convert jpg file to numpy array.
-Resize and save image file. 
-"""
-
-"""
-Create and training network
-Save trained model
-"""
-
-"""
-Get image file
-"""
+                callbacks=[TensorBoard(log_dir='./logs/log.log')])
 
